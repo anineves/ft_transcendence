7987d728c0749139bc39fa3bc6e85b0d0be54aa2
Ana Isabel Neves <anineves5@gmail.com>
2024-07-19 14:33:18 +0100

    teste

--- front-endSPA/backend/myproject/settings.py
+++ front-endSPA/backend/myproject/settings.py
@@ -53,7 +53,16 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+ALLOWED_HOSTS = ['*']
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
+    'http://0.0.0.0:5500',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'myproject.urls'
 
