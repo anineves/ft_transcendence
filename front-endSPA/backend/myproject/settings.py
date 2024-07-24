6014bc5b31852c299c4f27a8e2cda16cd7dfd9f2
Ana Neves <anineves5@gmail.com>
2024-07-24 17:10:43 +0100

    improve style

--- front-endSPA/backend/myproject/settings.py
+++ front-endSPA/backend/myproject/settings.py
@@ -90,12 +90,25 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'accounts',
+        'USER': 'user_accounts',
+        'PASSWORD': 'pw_accounts',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
