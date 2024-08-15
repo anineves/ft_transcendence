211e0089ecbdfd53697eb4e99572b52afea57906
abenjii <alexfern@student.42porto.com>
2024-08-15 19:29:49 +0100

    f syntax error removed

--- authentication/auth/myproject/urls.py
+++ authentication/auth/myproject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-ffrom django.contrib import admin
+from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
