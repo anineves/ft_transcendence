5cd11ae7f9f9c9fa1063eaa1120730404c1ef458
jpedroegger <jpedroegger@gmail.com>
2024-08-29 21:47:15 +0100

    Added endpoint for globalchat

--- authentication/auth/presence/routing.py
+++ authentication/auth/presence/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/presence/(?P<group_name>\w+)/$", consumers.PresenceConsumer.as_asgi()),
+    re_path(r"ws/global_chat/", consumers.ChatConsumer.as_asgi()),
     re_path(r"ws/pong_match/(?P<pong_match>\w+)/$", consumers.PongConsumer.as_asgi()),
 ]
