3c339394616c510a93768cf01289f8e5041da3a2
abenjii <alexfern@student.42porto.com>
2024-10-12 17:47:06 +0100

    Automitizes logs in kibana already

--- elk/kibana/config/script.sh
+++ elk/kibana/config/script.sh
@@ -2,16 +2,13 @@
 
 echo "Waiting Kibana initialize..."
 
-while ! curl -s http://localhost:5601 > /dev/null; do
-    sleep 15
-    echo "Trying to connect to Kibana..."
-done
+echo "Trying to connect to Kibana..."
 
 echo "Kibana running, creating data view..."
 
-curl -X POST "http://localhost:5601/api/data_views/data_view" \
-    -H "kbn-xsrf: true" \
-    -H "Content-Type: application/json" \
+curl -X POST https://localhost:5601/api/data_views/data_view \
+    -H "Content-Type: application/json; Elastic-Api-Version=2023-10-31" \
+    -H "kbn-xsrf: string" \
     -d '{
         "data_view": {
             "title": "logstash-*",
